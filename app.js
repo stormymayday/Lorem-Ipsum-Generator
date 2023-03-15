277c7fd3ea9eef18156b8091dc45c829768c9bef
stormymayday <stormymayday7@gmail.com>
2023-03-15 11:44:39 -0600

    Displaying random paragraph instead of the first one by passing in the 'random' variable instead of [0].

--- app.js
+++ app.js
@@ -31,14 +31,13 @@ form.addEventListener('submit', function (event) {
 
     // Random number between 0 and 8
     const random = Math.floor(Math.random() * text.length);
-    console.log(random);
 
     // Displaying one paragraph if the value is:
     // 1. NaN (The number input is empty)
     // 2. A negative integer
     // 3. Is greater that 9 (length of the text array)
     if (isNaN(value) || value < 0 || value > 9) {
-        result.innerHTML = `<p class='result'>${text[0]}</p>`;
+        result.innerHTML = `<p class='result'>${text[random]}</p>`;
     }
 
 });
